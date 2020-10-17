d65dd7c0de94f9a51824a6868006c4ca86f46078
iamcho2 <thatgrass@naver.com>
2020-10-17 17:34:14 +0900

    add week-2 project file

---
+++ week-2/seminar/seminar-2nd/seminar-2nd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seminar-2nd
+//
+//  Created by 초이 on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
