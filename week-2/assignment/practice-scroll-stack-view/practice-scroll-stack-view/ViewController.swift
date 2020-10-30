c2b58cdc5398a44aa24f7915fd8c7016a558534e
iamcho2 <thatgrass@naver.com>
2020-10-30 14:40:31 +0900

    add assignment

---
+++ week-2/assignment/practice-scroll-stack-view/practice-scroll-stack-view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice-scroll-stack-view
+//
+//  Created by 초이 on 2020/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
